0e94913f2728a71066dea95ab205ba8bc7e6ae1e
Melissa Cook <mkpspeedylegs@gmail.com>
2022-11-14 16:02:36 -0700

    2.6.1

--- 2.6/employeeManagerTest.test.ts
+++ 2.6/employeeManagerTest.test.ts
@@ -1,4 +1,4 @@
-import {Builder,By, Capabilities, until, WebDriver, } from "selenium-webdriver";
+import {Builder,By, Capabilities, until, WebDriver, WebElement} from "selenium-webdriver";
   const chromedriver = require("chromedriver");
 
   const driver: WebDriver = new Builder()
@@ -8,26 +8,50 @@ import {Builder,By, Capabilities, until, WebDriver, } from "selenium-webdriver";
   export class emPage {
       driver: WebDriver;
       url: string = "https://devmountain-qa.github.io/employee-manager/1.2_Version/index.html";
-
+      header: By = By.css('.titleText');
+      addEmployee: By = By.css('[name="addEmployee"]');
+      newEmployee: By = By.css('[name = "employee11"]');
+      nameInput: By = By.name('nameEntry');
+      phoneInput: By = By.name('phoneEntry');
+      titleInput: By = By.name('titleEntry');
         //FILL OUT LOCATORS CONSTRUCTOR AND METHODS IN ORDER TO PASS THE TEST
 
-        constructor(driver: WebDriver, url: string){
+        constructor(driver: WebDriver){
             this.driver = driver
-            this.url = url
         };
-        
+
         async navigate(){
-            await this.driver.get(this.url)
-        };
-
-        header: By = By.className('titleText');
-        addEmployee: By = By.name('addEmployee');
-        newEmployee: By = By.name('employee11');
-        nameInput: By = By.name('nameEntry');
-        phoneInput: By = By.name('phoneEntry');
-        titleInput: By = By.name('titleEntry');
-
+            await this.driver.get(this.url);
+            await this.driver.wait(until.elementLocated(this.header))
+        }
+       async click(elementBy: By,){
+            await this.driver.wait(until.elementLocated(elementBy))
+            return (await this.driver.findElement(elementBy)).click()
+       } 
+       async sendKeys(elementBy: By, keys){
+            await this.driver.wait(until.elementLocated(elementBy))
+            return this.driver.findElement(elementBy).sendKeys(keys)
+       }
+       async getText (elementBy){
+            await this.driver.wait(until.elementLocated(elementBy))
+            return this.driver.findElement(elementBy).getText()
+       }
+       async setInput(elementBy: By, keys:any):Promise<void>{
+            let input = await this.driver.findElement(elementBy)
+            await input.click()
+            await input.clear()
+            return input.sendKeys(keys)
+    
+       }
   }
+
+  const emPage = new employeePO()
+
+  describe("Practicing pageObjects", () => {
+    beforeEach(async ()=>{
+        await emPage.navigate()
+    })
+  })
 
 
   describe("Employee Manger Test", () => {
